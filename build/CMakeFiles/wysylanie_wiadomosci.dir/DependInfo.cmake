ebb7c5adc02d85de74040fb2587218e30bd6a1b5
Jakub Kusz <jakub.kusz@onet.pl>
2022-05-27 22:02:36 +0200

    kjk

---
+++ build/CMakeFiles/wysylanie_wiadomosci.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/src/Dr3D_gnuplot_api.cpp" "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/build/CMakeFiles/wysylanie_wiadomosci.dir/src/Dr3D_gnuplot_api.cpp.o"
+  "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/src/dron.cpp" "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/build/CMakeFiles/wysylanie_wiadomosci.dir/src/dron.cpp.o"
+  "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/src/figury_geometryczne.cpp" "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/build/CMakeFiles/wysylanie_wiadomosci.dir/src/figury_geometryczne.cpp.o"
+  "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/src/macierz_obr.cpp" "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/build/CMakeFiles/wysylanie_wiadomosci.dir/src/macierz_obr.cpp.o"
+  "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/src/main.cpp" "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/build/CMakeFiles/wysylanie_wiadomosci.dir/src/main.cpp.o"
+  "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/src/scena.cpp" "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/build/CMakeFiles/wysylanie_wiadomosci.dir/src/scena.cpp.o"
+  "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/src/uklad_wspolrzednych.cpp" "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/build/CMakeFiles/wysylanie_wiadomosci.dir/src/uklad_wspolrzednych.cpp.o"
+  "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/src/wektor.cpp" "/home/kubus/Pulpit/PULPIT_Z_UBUNTU/ProgramowanieObiektoweC++/programowanie-obiektowe-projekt/zad_Dron/build/CMakeFiles/wysylanie_wiadomosci.dir/src/wektor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
